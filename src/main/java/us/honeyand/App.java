b6b47ac0d381fe37e0facc1b83b775174d9ee55d
Eralp Şahin <eralpsahin@me.com>
2017-03-28 23:39:17 +0300

    Initial Commit

---
+++ src/main/java/us/honeyand/App.java
@@ -0,0 +1,13 @@
+package us.honeyand;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
