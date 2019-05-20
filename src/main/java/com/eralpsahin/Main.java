8622e3dae9d51cce86ab1965b3e35e2615ab491a
Eralp Sahin <eralpsahin@me.com>
2019-05-19 21:13:00 -0400

    Add app icon

--- src/main/java/com/eralpsahin/Main.java
+++ src/main/java/com/eralpsahin/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -21,7 +22,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
-
+        primaryStage.getIcons().add(new Image("/app_icon.png"));
         primaryStage.setTitle("Akari Solver");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
